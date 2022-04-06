30c7d34c26c8b0dae35b35ed2db670c124afefd0
Christopher Woodall <woodall.christopher@gmail.com>
2022-04-06 05:58:26 -0400

    Check if the container exists

--- ansible/roles/dnsmasq/files/containers/run.sh
+++ ansible/roles/dnsmasq/files/containers/run.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-docker run \
-  -d \
-  --rm \
-  --privileged \
-  --name dnsmasq \
-  --net=host \
-  -v /srv/containers/dnsmasq/config:/config \
-  dnsmasq-arm64:latest
+CONTAINER_NAME="dnsmasq"
+CONTAINER_EXIST=`docker ps --all --quiet --filter name="${CONTAINER_NAME}"`
 
-#  --cap-add=NET_ADMIN \
+
+if [ "${CONTAINER_EXIST}" ]; then
+  echo "Container '${CONTAINER_NAME}' Exist"
+
+  # if [ "$(docker ps -aq -f status=exited -f name=${CONTAINER_NAME})" ]; then
+  #   # Cleanup
+  #   docker rm "${CONTAINER_NAME}"
+  # fi
+
+  docker start "${CONTAINER_NAME}"
+
+else
+  docker run \
+    -d \
+    --privileged \
+    --net host \
+    --name dnsmasq \
+    -v /srv/containers/dnsmasq/config:/config \
+    dnsmasq-arm64:latest
+fi
+
